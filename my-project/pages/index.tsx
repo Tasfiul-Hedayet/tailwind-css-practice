a44e470e3ec278fa4a0fd325efefe7763a1accc4
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-11-08 15:08:39 +0600

    Alhamdulillah, project testing done

---
+++ my-project/pages/index.tsx
@@ -0,0 +1,22 @@
+
+export default function Home() {
+  return  <>
+  {/* link with home page  */}
+     <a href="/category">Category </a>
+     <a href="/feedback"> Feedback </a>
+     
+     <a href="/payment"> Payment </a>
+    
+     <a href="/admin"> Admin </a>
+   
+     <div className="my-8 ..."> <h1>Welcome to NSU Cafe Digitaliztion system </h1> </div>
+
+     <button className="bg-sky-500/100 ...">hello</button>
+     <button className="bg-blue-500 md:bg-green-500 ...">Subscribe</button>
+
+
+<div className="border-8 border-indigo-600 ..."></div>
+
+ </>
+}
+
